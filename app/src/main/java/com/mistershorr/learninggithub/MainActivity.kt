d96688e636b299294be88480680a9c418bce0a22
gshorr <gshorr@spusd.net>
2022-09-08 09:23:21 -0700

    added key x variable

--- app/src/main/java/com/mistershorr/learninggithub/MainActivity.kt
+++ app/src/main/java/com/mistershorr/learninggithub/MainActivity.kt
@@ -7,5 +7,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // very important variable
+        val x = 10
     }
 }
